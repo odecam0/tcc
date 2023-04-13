bb94462e624b5ff16d3a757a3652a90b5e54b838
Bruno Macedo <macedobruno@id.uff.br>
2023-04-13 20:28:13 -0300

    better Plot function

--- src/arpc_plot.py
+++ src/arpc_plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 from pdb import set_trace
 
@@ -40,55 +41,150 @@ def plot_all(df, save=False, participantes=[]):
         else:
             plt.show()
 
+import re
 
-# arpo -> Arpc Object -> Activity Recognition Protocol Object
-def plot_compare_side_err_bar(arpo, get_data_proc, show=True, gray=False, fig_ax=None, eb_width=None, legend=True):
+def group_labels_by_first_word(labels):
     """
-    Deve haver um padrão para os argumentos que GET_DATA_PROC deve receber e oque deve retornar
-    Receberá ARPO, e deve retornar uma lista com listas que poossuem tuplas, com o valor da métrica
-    e o limite superior e inferior do intervalo de confiança.
+    Receives a simples list of labels and return them grouped and separated by the
+    first word in the label, considering word written in camel case. LikeThisWordHere.
+    Example:
+    Input -> [ label0, ..., labelN ]
+    Output \/
+    {
+      'FirstWord0': ['RestOfLabel0', ..., 'RestOfLabelM_1'],
+       ...,
+      'FirstWordN': ['RestOfLabel0', ..., 'RestOfLabelM_k']
+    }
     """
+    list_of_all_first_words = []
+    for label in labels:
+        first_word_match = re.search('[A-Z][a-z]+', label)
+        first_word       = first_word_match.group()
+        if first_word not in list_of_all_first_words:
+            list_of_all_first_words += [first_word]
+    
+    # Initializing each group as an enpty list in the dictionary
+    groups = {}
+    for word in list_of_all_first_words:
+        groups[word] = []
+        
+    # Filling each group in the dictionary
+    for label in labels:
+        first_word_match = re.search('[A-Z][a-z]+', label)
+        first_word       = first_word_match.group()
+        rest_of_label    = re.sub(first_word, '', label)
+        groups[first_word] += [rest_of_label]
+    
+    return groups
 
-    # Experiments
+
+def plot_compare_err_bar(arpo, get_data_proc, grouping_scheme_proc):
+    """
+    GET_DATA_PROC is a procedure receives an Arpc object as parameter and returns a 3-tuple with
+    an array of means of some metric for each label, array of superior and inferior limits of error bar,
+    and and array of experiment names. Like this:
+        ( [ mean0, ... , meanN] ,
+        [ [inf0, ..., infN], [sup0, ..., supN] ],
+        [ <exp_name0, ..., exp_nameM>] )
+    One such procedure is arpc_plot.get_compare_side_err_barr_data defined in this same module.
+
+    LABEL_GROUPING_PROC will receive a simple list of labels and will return a list of lists with the
+    same labels grouped. For example:
+        Input --> [ label0, ..., label8]
+        Output \/
+    {
+        'FirstWord0': ['RestOfLabel0', ..., 'RestOfLabelM_1'],
+        ...,
+        'FirstWordN': ['RestOfLabel0', ..., 'RestOfLabelM_k']
+    }
+    """
+    # Experiments data
     values, err_bars, exp_labels = get_data_proc(arpo)
 
-    set_trace()
-    # Plotting code
     labels = arpo.confusion_matrixes[0][1]
-    if fig_ax:
-        fig, ax = fig_ax
-    else:
-        fig, ax = plt.subplots()
-    plots = []
+    grouped_labels = grouping_scheme_proc(labels)
 
-    exp_num = values.shape[0]
-    x = np.arange(len(labels))
-    width = .5 / exp_num
+    exp_nums = values.shape[0]
 
-    # exps[i][1] deve ser um array com shape (2, N) com o valor que somado
-    # ao valor de exps[i][0] vai resultar no limite inferior e superior do intervalo de confiança
-    for i in range(exp_num):
-        plot_kwargs = {'fmt':'o'}
-        if gray:
-            plot_kwargs['c'] = 'gray'
-        if eb_width:
-            plot_kwargs['elinewidth'] = eb_width
-        if legend:
-            plot_kwargs['label'] = exp_labels[i]
+    fig = plt.figure(figsize=(12,6))
+    gs = plt.GridSpec(1, len(grouped_labels), figure=fig)
 
-        this_x = x - (exp_num*width)/2 + i*width
-        plots += [ax.errorbar(this_x, values[i], err_bars[i], **plot_kwargs)]
+    colors_error_bar = list(plt.cm.Dark2(np.linspace(0, 1, exp_nums)))
 
-    ax.set_ylabel('Acurácias')
-    ax.set_title('Acurácia média de experimentos agrupada por classe')
-    ax.set_xticks(x, labels, rotation=45, ha='right')
 
-    ax.legend()
+    # This loop will create a sub gridspec for each label group to make members of the group closer
+    # for each gridspec created, it will create its children subplot, one for each full label of the group
+    # and for each subplot, a plot will be made for each experiment. These are the 3 nested loops.
+    for i, group in enumerate(grouped_labels):
+        sub_gs = gs[0, i].subgridspec(1, len(grouped_labels[group]))
 
-    fig.tight_layout()
+        # Adding title to each gridspec that separates a group
+        ax = fig.add_subplot(sub_gs[:])
+        ax.axis('off')
+        ax.set_title(group, fontsize=15)
 
-    if show:
-        plt.show()
+        for j, label in enumerate(grouped_labels[group]):
+
+            ax = fig.add_subplot(sub_gs[0, j])
+
+            ax.spines[['right', 'top', 'left']].set_visible(False)
+            ax.set_yticks([])
+            ax.set_xticks([0.5], [label], fontsize=12)
+            ax.set_xlim(-0.5, 1.5)
+            ax.set_ylim(-0.05, 1)
+
+            for exp in range(exp_nums):
+                # To get space between the experiments in each 
+                x = exp * (1/ (exp_nums - 1))
+
+                # values comes as an array with 1 array for each experiment
+                # each experiment array comes with <number of labels> floating numbers.
+                # Like this:
+                #  [ [exp0_value0, ... , exp0_valueN], ... , [expM_value0, ... , expM_valueN] ]
+                # And we want to plot the (i * exp_nums + j)th value in the j_th axs of the i_th gridspec
+                this_index = i * exp_nums + j
+                this_value = values[exp, this_index]
+                this_yerr = np.reshape(err_bars[exp, :, this_index], (2, 1))
+
+                ax.errorbar(x, this_value, this_yerr, c=colors_error_bar[exp], fmt='o', label=exp_labels[exp])
+
+                # if exp == exp_nums - 1 and i == len(grouped_labels) - 1 and j == len(grouped_labels[group]) - 1:
+                #     set_trace()
+
+    # Here ax will refer to the last subplot axis created by the previous loop, wich is enough to
+    # build a legend for us.
+    handles, _ = ax.get_legend_handles_labels() # We did not pass a label argument when plotting
+    # fig.legend(handles, exp_labels) # We will use the labels returned by GET_DATA_PROC
+    ax.legend() # We will use the labels returned by GET_DATA_PROC
+
+
+    # Here, as the final step, we create a subplot that covers all the parent gridspec, and use
+    # it to set a title for the whole figure, and draw the left ticks of the entire figure, that
+    # are colored based on how good is the accuracy, also drawing an horizontal line into the whole
+    # figure, also colored.
+    ax = fig.add_subplot(gs[:]) #     \/ Here the Title is hardcoded, it should be changed. !TODO!
+
+    # Create colormap from red to green, going through orange. And get 5 colors corresponding to
+    # 0%, 25%, 50%, 75%, 100% of accuracy.
+    red_to_green = LinearSegmentedColormap.from_list('GreenToRed', ['red', 'orange', 'green'])
+    percentage_values = np.linspace(0, 1, 5, endpoint=True)
+    red_to_green_values = red_to_green(percentage_values)
+
+    [t.set_visible(False) for t in ax.yaxis.get_ticklines()]
+    [t.set_color(red_to_green_values[i%5]) for i, t in enumerate(ax.yaxis.get_ticklabels())]
+    [ax.axhline(y=y, color=c, linestyle='--', alpha=0.5, linewidth=0.7)
+     for y, c in zip(percentage_values, red_to_green_values)]
+
+    ax.set_title("Acurácia média de diferentes modelos por atividade e intensidades", y=1.08)
+    ax.set_ylim(-0.05, 1) # Has to be the same as the previous subplots
+    ax.set_yticks(percentage_values, ['$'+str(int(i*100))+'\%$' for i in percentage_values], fontweight=0.5)
+    ax.set_xticks([])
+    ax.spines[['right', 'top', 'left', 'bottom']].set_visible(False)
+    ax.patch.set_alpha(0) # transparent background
+
+
+    plt.savefig('lstm_plot_compare_err_bar.png')
+
 
 def get_compare_side_err_barr_data(arpo, depth, metric_func, get_label_func):
     """
@@ -119,7 +215,6 @@ def get_compare_side_err_barr_data(arpo, depth, metric_func, get_label_func):
     #                          . . .
     #   [[ mean0, inf0, sup0 ], ..., [meanN, infN, supN]] ] # <- Last experiment
     for e in arpo.exp_gen(depth):
-        set_trace()
         exps += [[metric_func(e.confusion_matrixes, l) for l in get_label_func(arpo)]]
         exp_names += [e.name]
 
